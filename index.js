70e8e001d41361732b199831a29ba015e894d4de
Mahmoud Nagdy <mahmoudnagdy65@gmail.com>
2025-07-27 17:00:14 +0300

    first

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ dotenv.config()
 import cors from 'cors'
 import { bootstrap } from './src/index.router.js'
 const app = express()
-const PORT = 5000
 app.use(cors())
 
 bootstrap(app, express)
 
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ..... ${PORT}`);
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`Server is running on port ..... ${process.env.PORT}`);
 })
